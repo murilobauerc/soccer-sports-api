624f214fb271d85d2cfd215e26c83a6336a516ca
murilobauerc <murilo.bauer@sumup.com>
2023-05-06 16:13:36 -0300

    🚀 feat(prod.exs): add support for System.get_env("APP_NAME") environment variable in url configuration 🐛 fix(prod.exs): change default port from 4000 to System.get_env("PORT") or 4000

--- config/prod.exs
+++ config/prod.exs
@@ -5,7 +5,7 @@ config :logger, level: :info
 
 
 config :soccer_sports_api, SoccerSportsApiWeb.Endpoint,
-  http: [:inet6, port: System.get_env("PORT") || 4000]
+  http: [:inet6, port: System.get_env("PORT") || 4000],
   url: [host: System.get_env("APP_NAME") <> ".gigalixirapp.com", port: 443],
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE"),
   server: true
