ed41fd1c10cea28d24f138ef51b2908a93af5afe
murilobauerc <murilo.bauer@sumup.com>
2023-05-06 16:28:56 -0300

    🔒 chore(prod.exs): add force_ssl option to endpoint configuration ✨ feat(prod.exs): add cache_static_manifest option to endpoint configuration

--- config/prod.exs
+++ config/prod.exs
@@ -5,7 +5,9 @@ config :logger, level: :info
 
 
 config :soccer_sports_api, SoccerSportsApiWeb.Endpoint,
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
   http: [:inet6, port: System.get_env("PORT") || 4000],
   url: [host: System.get_env("APP_NAME") <> ".gigalixirapp.com", port: 443],
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE"),
+  cache_static_manifest: "priv/static/cache_manifest.json",
   server: true
