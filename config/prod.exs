0855f801a3ec0accb0c774cb2a0ce01a5d9d12b4
murilobauerc <murilo.bauer@sumup.com>
2023-05-06 16:03:55 -0300

    🔥 chore(prod.exs): remove cache_static_manifest configuration from production environment 🔧 chore(prod.exs): set logger level to info in production environment

--- config/prod.exs
+++ config/prod.exs
@@ -1,8 +1,5 @@
 import Config
 
-config :soccer_sports_api, SoccerSportsApiWeb.Endpoint,
-  cache_static_manifest: "priv/static/cache_manifest.json"
-
 # Do not print debug messages in production
 config :logger, level: :info
 
